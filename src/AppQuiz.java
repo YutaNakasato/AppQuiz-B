49446c595af8186ac4403fc36e8cf61ea3847134
Yuta Nakasato <yuta.nakasato.sys@gmail.com>
2017-06-27 11:44:28 +0900

    first commit

---
+++ src/AppQuiz.java
@@ -0,0 +1,28 @@
+public class AppQuiz {
+	public static void main(String[] args) {
+		int a = toi1();
+		int b = toi2();
+		int c = toi3();
+		int d = toi4();
+		int e = toi5();
+		int f = toi6();
+	}
+	public static int toi1(){
+		//裕	耶担当
+	}
+	public static int toi2(){
+		//中里
+	}
+	public static int toi3(){
+		//佐藤PDT大地@ダイブレッドマン
+	}
+	public static int toi4(){
+		//ヒロキ
+	}
+	public static int toi5(){
+		//マルチャン
+	}
+	public static int toi6(){
+		//カサイクン
+	}
+}
